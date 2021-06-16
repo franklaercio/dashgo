512e49525497a734a194e7418b49b191e80320bb
Frank Laercio <franklaercio@gmail.com>
2021-06-16 00:50:04 -0300

    Finished Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 
 export default function Header() {
@@ -52,6 +52,33 @@ export default function Header() {
 
         <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
+
+      <Flex 
+          align="center"
+          ml="auto"
+        >
+          <HStack 
+            spacing="8"
+            mx="8"
+            pr="8"
+            py="1"
+            color="gray.300"
+            borderRightWidth={1}
+            borderColor="gray.700"
+          >
+            <Icon as={RiNotificationLine} fontSize="20" />
+            <Icon as={RiUserAddLine} fontSize="20" />
+          </HStack>
+
+          <Flex align="center">
+            <Box mr="4" textAlign="right">
+              <Text>Frank Laércio</Text>
+              <Text color="gray.300" fontSize="small">frank@email.com</Text>
+            </Box>
+            
+            <Avatar size="md" name="Frank Laércio" src="https://github.com/franklaercio.png"/>
+          </Flex>
+        </Flex>
     </Flex>
   )
 }
