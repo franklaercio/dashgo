5077333edab270be9ff5e3216d4cf73bcc98d51b
Frank <franklaercio@gmail.com>
2021-08-31 23:25:39 -0300

    Adding user hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,30 +6,10 @@ import { RiAddLine } from 'react-icons/ri';
 import Pagination from '../../components/Pagination';
 import Link from 'next/link';
 import { useEffect } from 'react';
-import { useQuery } from 'react-query';
-import { api } from '../../services/api';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching , error } = useQuery('users', async () => {
-    const { data } = await api.get('users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: new Date(user.create_at).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5
-  });
+  const { data, isLoading, isFetching , error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
