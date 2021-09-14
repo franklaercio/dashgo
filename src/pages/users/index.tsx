a8f8243773863d17268781a52fe7996552083cb7
Frank Laércio <franklaercio@gmail.com>
2021-09-13 23:12:23 -0300

    Adding server side rendering

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,14 +22,17 @@ import Header from "../../components/Header";
 import { RiAddLine } from "react-icons/ri";
 import Pagination from "../../components/Pagination";
 import NextLink from "next/link";
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers(page);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -135,4 +138,16 @@ export default function UserList() {
       </Flex>
     </Box>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+
+  
+  return {
+    props: {
+
+    }
+  }
 }
